ae2685b2f49b2016b1415459b29d655e2a6f7111
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-11-23 17:05:42 +0300

    Düzenleme

--- 1-typescript/src/tutorial.tsx
+++ 1-typescript/src/tutorial.tsx
@@ -1,7 +1,17 @@
-import React from "react";
-
 function tutorial() {
   return <div>tutorial</div>;
 }
 
 export default tutorial;
+
+let tax: number | string = 10;
+tax = 100; // Geçerli
+tax = "$10"; // Geçerli
+
+console.log(tax); // Use the value of tax
+
+let loading: "pending" | "success" | "error" = "pending";
+loading = "success"; // Geçerli
+loading = "error"; // Geçerli
+
+console.log(loading); // Son durumda: "error"
