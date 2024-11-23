8cbf4c87ded58966406b52ac984b3aad11ff203d
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-11-23 17:21:54 +0300

    Basit Tip tanımları

--- 1-typescript/src/tutorial.tsx
+++ 1-typescript/src/tutorial.tsx
@@ -15,3 +15,27 @@ loading = "success"; // Geçerli
 loading = "error"; // Geçerli
 
 console.log(loading); // Son durumda: "error"
+
+// let notSure: any = 4;
+let notSure: unknown = 4;
+notSure = "maybe a string instead";
+notSure = false; // Geçerli
+
+console.log(notSure);
+
+const books: string[] = ["1984", "Brave New World", "Fahrenheit 451"];
+const foundBook: string | undefined = books.find((book) => book === "1984");
+// const foundBook: string | undefined = books.find((book) => book === "1984");
+console.log(foundBook?.length);
+
+console.log(books[0]); // 1984
+
+let discounter: number | null = null;
+discounter = 10;
+console.log(discounter);
+
+let prices: (string | number)[] = [1, 2, 3];
+
+prices = ["$1", "$2", "$3", 2 * 2];
+
+console.log(prices);
