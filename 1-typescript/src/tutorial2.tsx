7b6bd1671c3090dbdfbe789348128036ceea578a
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-11-24 16:59:06 +0300

    İsteğe bağlı

--- 1-typescript/src/tutorial2.tsx
+++ 1-typescript/src/tutorial2.tsx
@@ -4,21 +4,32 @@ interface Message {
   time: string;
 }
 
-displayMessage({ text: "Hello", from: "John", time: "10:00" });
+// displayMessage({ text: "Hello", from: "John", time: "10:00" });
 
-export default function displayMessage(message: Message) {
-  console.log(message.text);
-  console.log(message.from);
-  console.log(message.time);
-}
+// export default function displayMessage(message: Message) {
+//   console.log(message.text);
+//   console.log(message.from);
+//   console.log(message.time);
+// }
 
 interface Message {
   text: string;
   sender: string;
 }
 
-function displayMessage1({ text, sender }: Message) {
+export default function displayMessage1({ text, sender }: Message) {
   console.log(`Message from ${sender}: ${text}`);
 }
 
 displayMessage1({ sender: "Christopher", text: "hello, world" });
+
+let addThreeNumbers = (x: number, y: number, z?: number): number => {
+  if (z === undefined) {
+    return x + y;
+  } else {
+    return x + y + z;
+  }
+};
+
+console.log(addThreeNumbers(1, 2));
+console.log(addThreeNumbers(1, 2, 3));
