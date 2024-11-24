7c09d033a1f3134b363114522629bf6bb168e8ce
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-11-24 16:08:30 +0300

    Alıştırma - İşlev oluşturma

--- 1-typescript/src/tutorial.tsx
+++ 1-typescript/src/tutorial.tsx
@@ -75,3 +75,22 @@ function addNumbers(x: number, y: number): number {
   return x + y;
 }
 addNumbers(1, 2);
+
+const sum = function (input: number[]): number {
+  let total: number = 0;
+  for (let i = 0; i < input.length; i++) {
+    if (isNaN(input[i])) {
+      continue;
+    }
+    total += Number(input[i]);
+  }
+  return total;
+};
+
+console.log(sum([1, 2, 3]));
+
+function displayAlert(message: string | number) {
+  alert("The message is " + message);
+}
+
+displayAlert("Hello");
