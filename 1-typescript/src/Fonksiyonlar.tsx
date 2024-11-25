37fb8ac49852dd8e8e086d3ca47432960a104d99
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-11-25 16:59:50 +0300

    Type Alias

--- 1-typescript/src/Fonksiyonlar.tsx
+++ 1-typescript/src/Fonksiyonlar.tsx
@@ -127,3 +127,61 @@ function processData(
 console.log(processData(10)); // Output: 100
 console.log(processData("Hello")); // Output: HELLO
 console.log(processData("Hello", { reverse: true })); // Output: OLLEH
+
+const john: { id: number; name: string; isActive: boolean } = {
+  id: 1,
+  name: "john",
+  isActive: true,
+};
+const susan: { id: number; name: string; isActive: boolean } = {
+  id: 1,
+  name: "susan",
+  isActive: false,
+};
+
+function createUser(user: { id: number; name: string; isActive: boolean }): {
+  id: number;
+  name: string;
+  isActive: boolean;
+} {
+  console.log(`Hello there ${user.name.toUpperCase()} !!!`);
+
+  return user;
+}
+
+type User = { id: number; name: string; isActive: boolean };
+
+const john: User = {
+  id: 1,
+  name: "john",
+  isActive: true,
+};
+const susan: User = {
+  id: 1,
+  name: "susan",
+  isActive: false,
+};
+
+function createUser(user: User): User {
+  console.log(`Hello there ${user.name.toUpperCase()} !!!`);
+  return user;
+}
+
+type StringOrNumber = string | number; // Type alias for string | number
+
+let value: StringOrNumber;
+value = "hello"; // This is valid
+value = 123; // This is also valid
+
+type Theme = "light" | "dark"; // Type alias for theme
+
+let theme: Theme;
+theme = "light"; // This is valid
+theme = "dark"; // This is also valid
+
+// Function that accepts the Theme type alias
+function setTheme(t: Theme) {
+  theme = t;
+}
+
+setTheme("dark"); // This will set the theme to 'dark'
