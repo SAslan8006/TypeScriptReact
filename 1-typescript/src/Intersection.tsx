6682c93f5b6d5c9396af7697ba040b3bc84c0e5d
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-11-26 13:44:38 +0300

    Interface - Methods (more options)

--- 1-typescript/src/Intersection.tsx
+++ 1-typescript/src/Intersection.tsx
@@ -128,3 +128,50 @@ const deepWorkc: Bookc = {
 deepWorkc.printAuthor();
 const resultc = deepWorkc.printTitle("is an awesome book");
 console.log(resultc);
+
+interface Bookd {
+  readonly isbn: number;
+  title: string;
+  author: string;
+  genre?: string;
+  // method
+  printAuthor(): void;
+  printTitle(message: string): string;
+  printSomething: (someValue: number) => number;
+}
+
+const deepWorkd: Bookd = {
+  isbn: 9781455586691,
+  title: "Deep Work",
+  author: "Cal Newport",
+  genre: "Self-help",
+  printAuthor() {
+    console.log(this.author);
+  },
+  printTitle(message) {
+    return `${this.title} ${message}`;
+  },
+  // first option
+  // printSomething: function (someValue) {
+  //   return someValue;
+  // },
+  // second option
+  printSomething: (someValue) => {
+    // "this" gotcha
+    console.log(deepWorkd.author);
+    return someValue;
+  },
+  // third option
+  // printSomething(someValue) {
+  //   return someValue;
+  // },
+  // alternative
+  // printAuthor: () => {
+  //   console.log(deepWork.author);
+  // },
+};
+console.log(deepWorkd.printSomething(34));
+
+deepWorkd.printAuthor();
+const resultd = deepWorkd.printTitle("is an awesome book");
+console.log(resultd);
