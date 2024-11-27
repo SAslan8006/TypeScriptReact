7a22cf2f7ffde81a5c85cbfa3aee673f3c0fe9f9
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-11-27 14:21:26 +0300

    AxiosResponse Generics Tutorial

--- 1-typescript/src/types/GenericsFundamentals.tsx
+++ 1-typescript/src/types/GenericsFundamentals.tsx
@@ -1,3 +1,9 @@
+import axios, {
+  AxiosRequestConfig,
+  AxiosResponseHeaders,
+  InternalAxiosRequestConfig,
+  RawAxiosResponseHeaders,
+} from "axios";
 import React from "react";
 
 function GenericsFundamentals() {
@@ -165,3 +171,25 @@ const storeNumbers: StoreData<number> = {
 const randomStuff: StoreData = {
   data: ["random", 1],
 };
+
+// data is located in the data property
+
+const { data } = axios.get(someUrl);
+
+axios.get<{ name: string }[]>(someUrl);
+
+export class Axios {
+  get<T = any, R = AxiosResponse<T>, D = any>(
+    url: string,
+    config?: AxiosRequestConfig<D>
+  ): Promise<R>;
+}
+
+export interface AxiosResponse<T = any, D = any> {
+  data: T;
+  status: number;
+  statusText: string;
+  headers: RawAxiosResponseHeaders | AxiosResponseHeaders;
+  config: InternalAxiosRequestConfig<D>;
+  request?: any;
+}
