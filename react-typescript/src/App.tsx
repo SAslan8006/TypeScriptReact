313bf538ef716f36585a231d616ff9f91aba9ef0
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-11-29 15:10:04 +0300

    Challenge

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,7 +1,8 @@
 // import Component from "./starter/01-return";
 // import Props from "./starter/02-props";
 // import State from "./starter/03-state";
-import Events from "./starter/04-events";
+// import Events from "./starter/04-events";
+import Challenge from "./starter/05-challenge";
 
 function App() {
   return (
@@ -9,7 +10,10 @@ function App() {
       {/* <Component /> */}
       {/* <Props id={1} name={"ahmet"}> Ahmet </Props> */}
       {/* <State /> */}
-      <Events />
+      {/* <Events /> */}
+      <Challenge type="basic" name="ahmet" />
+      <Challenge type="advanced" name="ahmet" email="a@b.com" />
+      <Challenge type="basic" name="ahmet" email="a@b.com" />
       {/* <h2>React & Typescript</h2> */}
     </main>
   );
--- react-typescript/src/starter/05-challenge/index.tsx
+++ react-typescript/src/starter/05-challenge/index.tsx
@@ -1,9 +1,16 @@
-function Component() {
+type PropsWithChildren = {
+  name: string;
+  type: "basic" | "advanced";
+  email?: string;
+};
+function Component(props: PropsWithChildren): JSX.Element | null | string {
+  const { name, type, email } = props;
+  const alertType = type === "basic" ? "alert-success" : "alert-danger";
   return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Challenge</h2>
-    </div>
+    <article className={`alert ${alertType}`}>
+      <h2>user: {name}</h2>
+      {email && <h2>email: {email}</h2>}
+    </article>
   );
 }
 export default Component;
