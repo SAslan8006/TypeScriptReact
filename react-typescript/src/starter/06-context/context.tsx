66733e5f1a1d81e43f9934cc691eb3d26fa3de5e
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-12-01 12:35:18 +0300

    Context Theme

--- react-typescript/src/starter/06-context/context.tsx
+++ react-typescript/src/starter/06-context/context.tsx
@@ -1,11 +1,12 @@
-import { useState, createContext, useContext } from "react";
+import { createContext, useContext } from "react";
 
-const ThemeProviderContext = createContext<string | undefined>(undefined);
+const ThemeProviderContext = createContext<{ name: string } | undefined>(
+  undefined
+);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  const [name, setName] = useState("light");
   return (
-    <ThemeProviderContext.Provider value="naaaa">
+    <ThemeProviderContext.Provider value={{ name: "susan" }}>
       {children}
     </ThemeProviderContext.Provider>
   );
--- react-typescript/src/starter/06-context/index.tsx
+++ react-typescript/src/starter/06-context/index.tsx
@@ -1,9 +1,22 @@
+import { ThemeProvider, useTheme } from "./context";
+
+function ParentComponent() {
+  return (
+    <ThemeProvider>
+      <Component />
+    </ThemeProvider>
+  );
+  return <Component />;
+}
+
 function Component() {
+  const context = useTheme();
+  console.log(context);
+
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>Context API</h2>
+      <h2>random component</h2>
     </div>
   );
 }
-export default Component;
+export default ParentComponent;
