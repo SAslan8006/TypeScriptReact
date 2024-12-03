b4e415dc69b64df1abfa2464f825abd179b6cb69
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-12-03 17:23:26 +0300

    Fetch Data

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -3,7 +3,9 @@
 // import State from "./starter/03-state";
 // import Events from "./starter/04-events";
 // import Challenge from "./starter/05-challenge";
-import Contex from "./starter/06-context";
+// import Contex from "./starter/06-context";
+// import Reducer from "./starter/07-reducers";
+import FetchData from "./starter/08-fetch-data";
 
 function App() {
   return (
@@ -15,7 +17,9 @@ function App() {
       {/* <Challenge type="basic" name="ahmet" /> */}
       {/* <Challenge type="advanced" name="ahmet" email="a@b.com" /> */}
       {/* <Challenge type="basic" name="ahmet" email="a@b.com" /> */}
-      <Contex />
+      {/* <Contex /> */}
+      {/* <Reducer /> */}
+      <FetchData />
       {/* <h2>React & Typescript</h2> */}
     </main>
   );
--- react-typescript/src/starter/08-fetch-data/index.tsx
+++ react-typescript/src/starter/08-fetch-data/index.tsx
@@ -1,8 +1,50 @@
+import { useEffect, useState } from "react";
+import { Tour } from "./types";
+
+const url = "https://www.course-api.com/react-tours-project";
 function Component() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [data, setData] = useState<Tour[]>([] as Tour[]);
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(
+          error instanceof Error ? error.message : "there was an error..."
+        );
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error : {error}</div>;
+  }
+  console.log(data);
   return (
     <div>
       <h2>React & Typescript</h2>
       <h2>Fetch Data</h2>
+      {data.map((tour) => {
+        return (
+          <div key={tour.id}>
+            <h2>{tour.name}</h2>
+          </div>
+        );
+      })}
     </div>
   );
 }
