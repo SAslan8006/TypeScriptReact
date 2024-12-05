2b4cf9e66a1f46c0b5d6ec2fda7276da6331a604
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-12-05 18:42:12 +0300

    Add Todo and Delete Todo

--- Todo-TailwindViteTs/src/App.tsx
+++ Todo-TailwindViteTs/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import TodoItem from "./component/TodoItem";
 import { dummyData } from "./data/todos";
+import AddTodoForm from "./component/AddTodoForm";
+import TodoList from "./component/TodoList";
 
 function App() {
   const [todos, setTodos] = useState(dummyData);
@@ -10,20 +11,32 @@ function App() {
       prevTodos.map((todo) => (todo.id === id ? { ...todo, completed } : todo))
     );
   }
+
+  function addTodo(title: string) {
+    setTodos((prevTodos) => [
+      {
+        id: prevTodos.length + 1,
+        title,
+        completed: false,
+      },
+      ...prevTodos,
+    ]);
+  }
+
+  function deleteTodo(id: number) {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  }
+
   return (
-    <main className="py-10 bg-red-50 h-screen space-y-5">
-      <h1 className="text-3xl font-bold text-center"> Your Todos</h1>
-      <div className="max-w-3xl mx-auto bg-slate-100 rounded-md p-4">
-        <div className="space-y-2">
-          {todos.map((todo) => (
-            <TodoItem
-              key={todo.id}
-              todo={todo}
-              onCompletedChange={setTodoCompleted}
-              onDelete={() => {}}
-            />
-          ))}
-        </div>
+    <main className="py-10 h-screen space-y-5 overflow-y-auto">
+      <h1 className="font-bold text-3xl text-center">Your Todos</h1>
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
+        <AddTodoForm onSubmit={addTodo} />
+        <TodoList
+          todos={todos}
+          onCompletedChange={setTodoCompleted}
+          onDelete={deleteTodo}
+        />
       </div>
     </main>
   );
