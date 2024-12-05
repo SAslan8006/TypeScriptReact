54f1408b0df27c110d68cd857a610126a4b1b837
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-12-05 15:53:17 +0300

    Düzenlme

--- Todo-TailwindViteTs/src/App.tsx
+++ Todo-TailwindViteTs/src/App.tsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <main className="py-10 bg-red-50 h-screen">
       <h1 className="text-3xl font-bold text-center"> Your Todos</h1>
+      <div> </div>
     </main>
   );
 }
