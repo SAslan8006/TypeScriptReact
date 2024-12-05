8d30ed16da01706ab69dcd1afe4be1f5794f13ef
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-12-05 16:51:15 +0300

    Component Props Gönderme vs

--- Todo-TailwindViteTs/src/App.tsx
+++ Todo-TailwindViteTs/src/App.tsx
@@ -1,8 +1,18 @@
+import TodoItem from "./component/TodoItem";
+import { dummyData } from "./data/todos";
+import { Todo } from "./types/todo";
+
 function App() {
   return (
-    <main className="py-10 bg-red-50 h-screen">
+    <main className="py-10 bg-red-50 h-screen space-y-5">
       <h1 className="text-3xl font-bold text-center"> Your Todos</h1>
-      <div> </div>
+      <div className="max-w-3xl mx-auto bg-slate-100 rounded-md p-4">
+        <div className="space-y-2">
+          {dummyData.map((todo) => (
+            <TodoItem key={todo.id} todo={todo} />
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
---
+++ Todo-TailwindViteTs/src/component/TodoItem.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Todo } from "../types/todo";
+
+interface TodoItemProps {
+  todo: Todo;
+}
+export default function TodoItem({ todo }: TodoItemProps) {
+  return (
+    <div className="flex items-center gap-2 border rounded-md p-2 border-gray-400 bg-white hover:bg-slate-50">
+      <input className="scale-125" type="checkbox" checked={todo.completed} />
+      <span className={todo.completed ? "line-through text-gray-400" : ""}>
+        {todo.title}{" "}
+      </span>
+    </div>
+  );
+}
