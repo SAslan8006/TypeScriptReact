04fef48db5c9ca2b6354df5bbe6c67011e1e43f5
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-12-12 10:56:35 +0300

    Düzenleme ProjeBaşlangıç

---
+++ reduxtoolkitTs/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAv3nrj2vp11JrtlpDMiv-e8LRsOPvG5JE",
+    authDomain: "reduxtoolkitts.firebaseapp.com",
+    projectId: "reduxtoolkitts",
+    storageBucket: "reduxtoolkitts.firebasestorage.app",
+    messagingSenderId: "272116917996",
+    appId: "1:272116917996:web:87c4630ba7e53aa402e7c9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- reduxtoolkitTs/vite.config.ts
+++ reduxtoolkitTs/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //Eğer Server port değiştirmek istersen burayı kullanabilirsin
+  server: {
+    open: true,
+    port: 3000
+  }
 })
