39839359ca98d518d6232ea1260bbe6a9510bd03
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-12-12 11:40:58 +0300

    sssss

--- reduxtoolkitTs/src/App.tsx
+++ reduxtoolkitTs/src/App.tsx
@@ -1,6 +1,14 @@
-function App() {
-  return;
-  <div className="max-w-screen-2xl mx-auto">Hello Word</div>;
+import { Button } from "./components/ui/button";
+
+function App(): JSX.Element {
+  return (
+    <div className="max-w-screen-2xl mx-auto">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- reduxtoolkitTs/src/firebase.js
+++ reduxtoolkitTs/src/firebase.js
@@ -1,17 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAv3nrj2vp11JrtlpDMiv-e8LRsOPvG5JE",
-    authDomain: "reduxtoolkitts.firebaseapp.com",
-    projectId: "reduxtoolkitts",
-    storageBucket: "reduxtoolkitts.firebasestorage.app",
-    messagingSenderId: "272116917996",
-    appId: "1:272116917996:web:87c4630ba7e53aa402e7c9"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export default app;
