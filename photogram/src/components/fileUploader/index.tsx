1914c994c2b10eadac637e29a736094fa0a94098
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-12-14 18:12:21 +0300

    FileUpload

---
+++ photogram/src/components/fileUploader/index.tsx
@@ -0,0 +1,16 @@
+import { FileUploaderRegular } from "@uploadcare/react-uploader";
+import "@uploadcare/react-uploader/core.css";
+
+function App() {
+  return (
+    <div>
+      <FileUploaderRegular
+        sourceList="local, url, camera, dropbox"
+        classNameUploader="uc-light"
+        pubkey="685f50edc277b5811ba6"
+      />
+    </div>
+  );
+}
+
+export default App;
--- photogram/src/types/index.tsx
+++ photogram/src/types/index.tsx
@@ -1,7 +1,37 @@
+import { OutputFileEntry } from "@uploadcare/blocks";
+
 export type UserLogIn = {
   email: string;
   password: string;
 };
 export type UserSignIn = UserLogIn & {
   confirmPassword: string;
+};
+
+export type FileEntry = {
+  files: OutputFileEntry[];
+};
+
+export type Post = {
+  caption: string;
+  photos: PhotoMeta[];
+  likes: number;
+  userlikes: [];
+  userId: string | null;
+  date: Date;
+};
+
+export type PhotoMeta = {
+  cdnUrl: string;
+  uuid: string;
+};
+
+export type DocumentResponse = {
+  id: string;
+  caption: string;
+  photos: PhotoMeta[];
+  likes: number;
+  userlikes: [];
+  userId: string | null;
+  date: Date;
 };
